d1c172c8e11cd2cb9ba114afe2461367397c0450
OmarEinea <eineao@gmail.com>
2014-11-20 17:52:29 +0400

    Remove some unnecessary stuff from modify-ramdisk.sh

--- GreatDevs/modify-ramdisk.sh
+++ GreatDevs/modify-ramdisk.sh
@@ -4,13 +4,12 @@
 mkdir /tmp/ramdisk
 cd /tmp/ramdisk
 gunzip -c ../boot.img-ramdisk.gz | cpio -i
-rm ../boot.img-ramdisk.gz
 
-# changing logo
+# change logo
 cp ../logo.rle .
 
-# replaceing current recovery with twrp recovery
-cp ../ramdisk-recovery.cpio sbin/ramdisk-recovery.cpio
+# replace current recovery with twrp recovery
+cp ../ramdisk-recovery.cpio sbin
 
 # unpack ramdisk
 mkdir sbin/ramdisk
@@ -24,10 +23,8 @@ sed -i '/stop mpdecision/d' init.qcom.rc
 
 # build new cpio ramdisk 
 find . | cpio -o -H newc > ../ramdisk.cpio
-cd ..
-rm -rf ramdisk
-
-cd ..
+cd ../..
+rm -rf sbin/ramdisk
 find . | cpio -o -H newc | gzip > ../ramdisk.gz
 
 exit 0
