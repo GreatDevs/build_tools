19d4ee7f060fae0e669926462e117aceed37a2ad
OmarEinea <eineao@gmail.com>
2014-11-20 18:40:10 +0400

    Move mkbootimg.sh contant to modify-ramdisk.sh -This'll make the process more compact -Stamp modify-ramdisk.sh and updater-scripts with GD

--- GreatDevs/modify-ramdisk.sh
+++ GreatDevs/modify-ramdisk.sh
@@ -27,4 +27,7 @@ cd ../..
 rm -rf sbin/ramdisk
 find . | cpio -o -H newc | gzip > ../ramdisk.gz
 
+# generate final boot.img
+./mkbootimg --kernel zImage --ramdisk ramdisk.gz --dt dt.img --cmdline "androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 msm_rtb.enable=0 lpj=192598 dwc3.maximum_speed=high dwc3_msm.prop_chg_detect=Y" --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x02000000 --tags_offset 0x01E00000 --output GDboot.img
+
 exit 0
